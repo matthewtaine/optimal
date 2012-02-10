37a0321fc7124d2785b2969c2979422fd7642c33
Matthew <matthew@optimalworkshop.com>
2012-02-10 13:23:05 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Optimal::Application.config.secret_token = 'bc8eb9af6b428a717b3feda40c10896a2f502785f91bf7d310bdc7b67b0dc3f412ecb167e0736800f476114f663cd2e4f87eae05bd3c1d9afda8bbb8d2898f3a'
