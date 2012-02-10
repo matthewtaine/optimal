294b2322984e4374a7af062f0951f613c6aeb636
Matthew <matthew@optimalworkshop.com>
2012-02-10 13:52:27 +1300

    Added Pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+# This controller defines a class called PagesController
+# It organises the methods for controlling page views
+# It inherits from the ApplicationContrller Class
+
+class PagesController < ApplicationController
+  
+  # Home method renders the view corresponding to the action
+  # Home action has a corresponding view called home.html.erb
+  def home
+  	# Actions
+  end
+	
+	# Pricing method
+  def pricing
+  	# Actions
+  end
+
+	# Contact method
+  def contact
+  	# Actions
+  end
+  
+end
